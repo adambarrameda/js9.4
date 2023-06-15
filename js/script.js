a532f29041aef4d51495df01a43e87c55de28b61
barrameda <hello@adambarrameda.com>
2023-06-14 20:30:23 -0400

    Dish assignment added per 9.5

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-// Skillcrush JS 9.4
-// Assemble a Potluck Guest List 
+// Skillcrush JS 9.4 + JS 9.5
+// Assemble a Potluck Guest List
+// Assign a Potluck Dish
 
 // Starter Code 
 // invite button
@@ -15,6 +16,10 @@ const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
 
+// 9.5 Select Assigned Items
+const assignButton = document.querySelector("button.assign");
+const assignedItems = document.querySelector(".assigned-items");
+
 // Add an Event Listener & Create an Element
 addGuestButton.addEventListener("click", function(e){
     const guest = guestInput.value;
@@ -26,6 +31,7 @@ addGuestButton.addEventListener("click", function(e){
         addToList(guest);
         clearInput();
         updateGuestCount();
+        guestInput.focus();
     }
 });
 
@@ -52,3 +58,37 @@ const updateGuestCount = function(){
         guestFull.classList.remove("hide");
     }
 };
+
+// 9.5 Build an Array + Select Elements & Loop Through the Array
+const assignItems = function(){
+    let potluckItems = 
+        ["Halloumi and Melon Skewers", 
+        "Peach and Prosciutto Bruschetta", 
+        "Veggie Nori Rolls",
+        "Caprese Skewers",
+        "3-Cheese Tomato Tart",
+        "Zoodles with Tomato and Corn",
+        "3-Bean Couscous Salad",
+        "Lemon Cream Icebox Cake",
+        "Pesto Pasta Salad",
+        "Tangy Carrot Slaw",
+        "Strawberry Icebox Cake",
+        "Sheet Pan Lemon Yogurt Tart",
+        "Grilled Zucchini Salad",
+        "Peach and Tahini Tart"];
+    const allGuests = document.querySelectorAll(".guest-list li");
+    for (let guest of allGuests) {
+        const randomPotluckIndex = Math.floor(Math.random()*potluckItems.length);
+        let randomPotluckItem = potluckItems[randomPotluckIndex];
+        let listItem = document.createElement("li");
+        listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}. (:`;
+        assignedItems.append(listItem);
+        potluckItems.splice(randomPotluckIndex, 1);
+    };
+};
+
+// Add an Event Listener & Update PotluckItems Array
+assignButton.addEventListener("click", function(){
+    assignItems();
+    assignButton.disabled = true;
+});
